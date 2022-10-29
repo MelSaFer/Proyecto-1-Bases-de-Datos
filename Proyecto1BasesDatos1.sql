37b03f286e0eee63f71681b5f409cabacb09794a
MelSaFer <dhnmelfer@estudiantec.cr>
2022-10-28 18:00:20 -0600

    Cambios archivo sql

--- Proyecto1BasesDatos1.sql
+++ Proyecto1BasesDatos1.sql
@@ -1,4 +1,15 @@
 /*
-Bases de Datos 1
-Proyecto 1
+	INSTITUTO TECNOLÓGICO DE COSTA RICA
+		CAMPUS TECNOLÓGICO CARTAGO
+	ESCUELA DE INGENIERIA EN COMPUTACION
+		BASES DE DATOS 1 - IC4301 
+				PROYECTO 1
+                
+	Profesora: Alicia Salazar Hernández
+    
+			   Estudiantes: 
+Melany Dahiana Salas Fernández - 2021121147
+	Moises Solano Espinoza - 2021144322
+    
+			II SEMESTRE, 2022
 */
